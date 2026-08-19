b45b18b3cf8404c9e593c17dfed62b9d89288d7b
Gabriel cunha <ggabrielcunha06@gmail.com>
2026-08-18 22:49:02 -0300

    feat: implementa login de usuarios

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { z } from "zod";
 import Usuario from "../models/Usuario.js";
+import jwt from "jsonwebtoken";
 
 export const registerSchema = z.object({
   nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
@@ -49,5 +50,100 @@ export const register = async (req: Request, res: Response): Promise<void> => {
   } catch (error) {
     console.error("Erro no registro:", error);
     res.status(500).json({ mensagem: "Erro interno do servidor ao processar o cadastro." });
+  }
+};
+
+export const loginSchema = z.object({
+  email: z.string().email("Formato de e-mail inválido"),
+  senha: z.string().min(1, "A senha é obrigatória")
+});
+
+export const login = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const parsedData = loginSchema.safeParse(req.body);
+
+    if (!parsedData.success) {
+      res.status(400).json({
+        mensagem: "Erro de validação nos campos fornecidos",
+        erros: parsedData.error.flatten().fieldErrors
+      });
+      return;
+    }
+
+    const { email, senha } = parsedData.data;
+
+    const usuario = await Usuario.findOne({
+      where: { email }
+    });
+
+    if (!usuario) {
+      res.status(401).json({
+        mensagem: "E-mail ou senha inválidos."
+      });
+      return;
+    }
+
+    if (!usuario.ativo) {
+      res.status(403).json({
+        mensagem: "Usuário desativado."
+      });
+      return;
+    }
+
+    const senhaValida = await bcrypt.compare(
+      senha,
+      usuario.senha
+    );
+
+    if (!senhaValida) {
+      res.status(401).json({
+        mensagem: "E-mail ou senha inválidos."
+      });
+      return;
+    }
+
+    const jwtSecret = process.env.JWT_SECRET;
+
+    if (!jwtSecret) {
+      console.error("JWT_SECRET não configurado.");
+
+      res.status(500).json({
+        mensagem: "Erro de configuração do servidor."
+      });
+
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        id_usuario: usuario.id_usuario,
+        tipo: usuario.tipo
+      },
+      jwtSecret,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    res.status(200).json({
+      mensagem: "Login realizado com sucesso!",
+      usuario: {
+        id_usuario: usuario.id_usuario,
+        nome: usuario.nome,
+        email: usuario.email,
+        tipo: usuario.tipo
+      },
+      token
+    });
+
+  } catch (error) {
+    console.error("Erro no login:", error);
+
+    res.status(500).json({
+      mensagem: "Erro interno do servidor ao realizar login."
+    });
   }
 };
